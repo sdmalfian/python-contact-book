e48e0069d173a25f94b785c32e26fae01b6b5e05
Sadam Alfian <sadamalfianp@gmail.com>
2022-05-25 23:46:01 +0700

    finished adding the CRUD functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 Contact Book Application By Sadam Alfian.
 """
+
 contacts = {
     1 : {
         "name" : "Emil",
@@ -19,7 +20,6 @@ contacts = {
     }
 }
 
-# Creating a menu display
 def showMenu():
     print("Welcome to Contact Books. What do you want to do?")
     print("1. View contact")
@@ -47,18 +47,17 @@ def selectMenu():
             print("\n==============")
             print("Change Contact")
             print("==============")
-            #updateContact()
-            #showMenu()
+            updateContact()
+            showMenu()
         elif option == 3:
             print("\n===========")
             print("Add Contact")
             print("===========")
-            #addContact()
-            #showMenu()
+            addContact()
+            showMenu()
         elif option == 4:
-            #deleteContact()
-            #showMenu()
-            print("4 selected")
+            deleteContact()
+            showMenu()
         elif option == 5:
             print("Good bye!")
             break
@@ -66,17 +65,79 @@ def selectMenu():
             print("Option unavailable.")
 
 def showContacts():
-    # manual numbering 
+    # Numbering
     num = 1
     print("================================================================================")
     print("|No.| Name            | Number           | Address:                            |")
     print("================================================================================")
     for contact in contacts.values():
-        # format to left-align and give some spacing
+        # format to left-align and give some space
         print("|{no} .| {name:<15} | {number:<15}  | {address:<35} |"
         .format(no = num, name = contact["name"], 
                 number = contact["number"], address = contact["address"]))
         num += 1
     print("================================================================================")
 
+def addContact():
+    # get the last key from the contacts and add 1 to it
+    contact_order = list(contacts.keys())[-1] + 1
+
+    # get input from the user to be saved as a new contact
+    while True:
+        try:
+            name = str(input("Enter contact name: "))
+            number = int(input("Enter contact number: "))
+            address = str(input("Enter contact address: "))
+            # add data to contacts
+            contacts[contact_order] = {
+                "name" : "{}". format(name),
+                "number" : number,
+                "address" : "{}".format(address)
+            }
+            print("Data " + name + " added succesfully!\n")
+            break
+        except ValueError:
+            print("Wrong input. Please enter a correct format.\n")
+            break
+
+def updateContact():
+    isUpdate = False
+    showContacts()
+    # get data from user
+    oldName = str(input("Enter contact name you want to change: "))
+    # check the name
+    for contact in contacts.values():
+        if oldName in contact.values():
+            print("Data " + oldName + " found!")
+            try:
+                newName = str(input("Enter contact new name: "))
+                newNumber = int(input("Enter contact number: "))
+                newAddress = str(input("Enter contact address: "))
+                # update contact
+                contact.update({"name" : "{}". format(newName)})
+                contact.update({"number" : newNumber})
+                contact.update({"address" : "{}". format(newAddress)})
+                isUpdate = True
+                print("Data '{}' succesfully changed to '{}'.\n".format(oldName, newName))
+                
+            except ValueError:
+                print("Format error. Please enter a correct number.") 
+
+    if isUpdate == False:
+        print("Data not found. Please enter a correct name\n")
+        showMenu()
+
+def deleteContact():
+    isDelete = False
+    showContacts()
+    name = str(input("Enter contact's name you want to delete: "))
+    for contact_id, contact_info in list(contacts.items()):
+        if contact_info["name"] == name:
+            del contacts[contact_id]
+            showContacts()
+            print("Data '{}' deleted Succesfully!\n".format(name))
+            isDelete = True
+    if isDelete == False: print("Data not found!\n")
+
+#code
 showMenu()
